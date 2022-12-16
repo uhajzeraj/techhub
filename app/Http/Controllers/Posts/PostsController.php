2928b7a94a8e341e63fa2c37096e3aaed6eb13d5
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-16 11:40:48 +0100

    find all posts and fill array with their contents

---
+++ app/Http/Controllers/Posts/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Posts;
+
+final class PostsController
+{
+    public function index()
+    {
+        $postsPath = storage_path('app/posts');
+
+        $postFiles = array_diff(scandir($postsPath), ['.', '..']);
+
+        $postsContent = [];
+
+        foreach ($postFiles as $postFile) {
+            $postsContent[] = file_get_contents($postsPath . '/' . $postFile);
+        }
+
+        return view('posts.index');
+    }
+
+    public function show(string $post)
+    {
+        return view('posts.show');
+    }
+}
