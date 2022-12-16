b98f9a85757349326f28c485e6aeb95e4ee87666
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-16 10:57:45 +0100

    show all posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,6 +10,64 @@
 
 <body>
     <h1>Hello world 👋🏻</h1>
+
+    <h3>My first post</h3>
+    <article>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu arcu nec nulla vestibulum efficitur
+        eget sit amet eros. Mauris eget efficitur libero. Mauris ac augue turpis. Donec ut gravida turpis, in vehicula
+        ex. Maecenas dui mi, eleifend vitae malesuada non, suscipit nec libero. Nam fringilla feugiat nunc quis
+        tincidunt. Integer semper aliquam erat, et dignissim metus porttitor vitae.
+
+        Ut eleifend tortor vel leo aliquet porttitor. Duis sagittis ullamcorper feugiat. Proin vitae tincidunt magna,
+        sed sollicitudin dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Morbi vitae venenatis libero. Sed aliquet ante ut mauris finibus, id venenatis lacus accumsan. Praesent
+        imperdiet laoreet sapien. Cras laoreet id mauris vel finibus. Aliquam mattis eget orci quis ornare. Aenean
+        dictum diam in metus iaculis pharetra. Phasellus interdum id nulla quis ornare.
+
+        Duis in nulla auctor, sodales felis vel, dapibus justo. Duis et neque vitae libero aliquam pulvinar. Sed finibus
+        erat id efficitur porttitor. Nullam velit felis, maximus nec hendrerit eu, pretium quis ligula. Duis tempus
+        tellus id ante suscipit finibus. Morbi et varius nisi. Quisque elit lacus, fringilla ut nibh nec, lobortis
+        mollis purus. Vestibulum nec dui sem. Sed vitae nulla urna. Suspendisse ultricies tellus ac ligula mollis, nec
+        ornare lacus sodales.
+    </article>
+    <h3>My second post</h3>
+    <article>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu arcu nec nulla vestibulum efficitur
+        eget sit amet eros. Mauris eget efficitur libero. Mauris ac augue turpis. Donec ut gravida turpis, in vehicula
+        ex. Maecenas dui mi, eleifend vitae malesuada non, suscipit nec libero. Nam fringilla feugiat nunc quis
+        tincidunt. Integer semper aliquam erat, et dignissim metus porttitor vitae.
+
+        Ut eleifend tortor vel leo aliquet porttitor. Duis sagittis ullamcorper feugiat. Proin vitae tincidunt magna,
+        sed sollicitudin dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Morbi vitae venenatis libero. Sed aliquet ante ut mauris finibus, id venenatis lacus accumsan. Praesent
+        imperdiet laoreet sapien. Cras laoreet id mauris vel finibus. Aliquam mattis eget orci quis ornare. Aenean
+        dictum diam in metus iaculis pharetra. Phasellus interdum id nulla quis ornare.
+
+        Duis in nulla auctor, sodales felis vel, dapibus justo. Duis et neque vitae libero aliquam pulvinar. Sed finibus
+        erat id efficitur porttitor. Nullam velit felis, maximus nec hendrerit eu, pretium quis ligula. Duis tempus
+        tellus id ante suscipit finibus. Morbi et varius nisi. Quisque elit lacus, fringilla ut nibh nec, lobortis
+        mollis purus. Vestibulum nec dui sem. Sed vitae nulla urna. Suspendisse ultricies tellus ac ligula mollis, nec
+        ornare lacus sodales.
+    </article>
+    <h3>My third post</h3>
+    <article>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu arcu nec nulla vestibulum efficitur
+        eget sit amet eros. Mauris eget efficitur libero. Mauris ac augue turpis. Donec ut gravida turpis, in vehicula
+        ex. Maecenas dui mi, eleifend vitae malesuada non, suscipit nec libero. Nam fringilla feugiat nunc quis
+        tincidunt. Integer semper aliquam erat, et dignissim metus porttitor vitae.
+
+        Ut eleifend tortor vel leo aliquet porttitor. Duis sagittis ullamcorper feugiat. Proin vitae tincidunt magna,
+        sed sollicitudin dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Morbi vitae venenatis libero. Sed aliquet ante ut mauris finibus, id venenatis lacus accumsan. Praesent
+        imperdiet laoreet sapien. Cras laoreet id mauris vel finibus. Aliquam mattis eget orci quis ornare. Aenean
+        dictum diam in metus iaculis pharetra. Phasellus interdum id nulla quis ornare.
+
+        Duis in nulla auctor, sodales felis vel, dapibus justo. Duis et neque vitae libero aliquam pulvinar. Sed finibus
+        erat id efficitur porttitor. Nullam velit felis, maximus nec hendrerit eu, pretium quis ligula. Duis tempus
+        tellus id ante suscipit finibus. Morbi et varius nisi. Quisque elit lacus, fringilla ut nibh nec, lobortis
+        mollis purus. Vestibulum nec dui sem. Sed vitae nulla urna. Suspendisse ultricies tellus ac ligula mollis, nec
+        ornare lacus sodales.
+    </article>
 </body>
 
 </html>
