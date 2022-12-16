62d4613d0b57374acdbc9db4379482770aa149e7
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-16 11:00:39 +0100

    add route definition

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home']);
 Route::get('/posts', [PostsController::class, 'index']);
+Route::get('/posts/first-post', [PostsController::class, 'showFirstPost']);
