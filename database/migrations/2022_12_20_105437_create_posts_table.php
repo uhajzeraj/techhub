61c029c44159a064182b28167aa07c41ad0c32cc
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-20 12:06:20 +0100

    add slug to posts table

--- database/migrations/2022_12_20_105437_create_posts_table.php
+++ database/migrations/2022_12_20_105437_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('author_id');
+            $table->string('slug');
             $table->string('title');
             $table->text('excerpt');
             $table->text('content');
