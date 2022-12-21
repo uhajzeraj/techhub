d0dcb9bc4cb237480218828abb05c92ef362b0f3
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-21 12:03:01 +0100

    use the same author for all the posts

--- database/seeders/PostsSeeder.php
+++ database/seeders/PostsSeeder.php
@@ -3,12 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 final class PostsSeeder extends Seeder
 {
     public function run(): void
     {
-        Post::factory(10)->create();
+        Post::factory(10)->create([
+            'author_id' => User::factory()->create(),
+        ]);
     }
 }
