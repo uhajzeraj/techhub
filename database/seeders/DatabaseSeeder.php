0458330714783d00d6806576b87c4f994b113e93
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-21 11:56:39 +0100

    also create some users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,7 +10,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(PostsSeeder::class);
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
