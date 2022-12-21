5154cb762a1befb30c88c7fd3e96ae1f366dcd24
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-21 10:50:19 +0100

    remove now-unused code

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -4,12 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Posts;
 
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
-use Spatie\YamlFrontMatter\Document;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 final class PostsController
 {
@@ -31,23 +26,5 @@ final class PostsController
         return view('posts.show', [
             'post' => $post,
         ]);
-    }
-
-    private function getPostsContent(): Collection
-    {
-        $postFiles = Storage::files('posts');
-
-        $postsContent = [];
-
-        foreach ($postFiles as $postFile) {
-            $postsContent[] = $this->parsePost($postFile);
-        }
-
-        return collect($postsContent);
-    }
-
-    private function parsePost(string $fileName): Document
-    {
-        return YamlFrontMatter::parse(Storage::get($fileName));
     }
 }
