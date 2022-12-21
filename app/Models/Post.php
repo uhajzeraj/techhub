33c2f299caba67b2c761f0cbad74f415f8e449e1
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-21 11:55:57 +0100

    use HasFactory trait in Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class Post extends Model
 {
+    use HasFactory;
 }
