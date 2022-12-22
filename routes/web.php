772caedbb9bb541683daf7f4ebfb6eea7fd7fb36
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-22 11:43:24 +0100

    add example GET /store-posts endpoint

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
-Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+Route::get('/store-post', [PostsController::class, 'store'])->name('posts.store');
