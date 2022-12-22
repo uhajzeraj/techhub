ddd92bf0595e0690a19b24f8e3e0b71fb87688d4
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-22 11:43:37 +0100

    add fake posts on controller action

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Posts;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 
 final class PostsController
 {
@@ -30,5 +31,16 @@ final class PostsController
 
     public function store()
     {
+        $words = fake()->words(6, true);
+
+        $post = Post::create([
+            'author_id' => 5,
+            'title' => ucfirst($words),
+            'slug' => Str::slug($words),
+            'excerpt' => fake()->paragraphs(3, true),
+            'content' => fake()->paragraphs(8, true),
+        ]);
+
+        return redirect()->route('posts.index');
     }
 }
