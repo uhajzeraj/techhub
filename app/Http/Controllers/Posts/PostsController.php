71cdeb7f972199d965482ac2272bf4935fbf4ca7
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-29 13:09:40 +0100

    add functionality for saving post

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers\Posts;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 final class PostsController
@@ -40,7 +42,24 @@ final class PostsController
         ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $title = $request->post('title');
+        $excerpt = $request->post('excerpt');
+        $content = $request->post('content');
+        $categoryId = $request->post('category_id');
+
+        $post = new Post();
+
+        $post->title = $title;
+        $post->slug = fake()->slug();
+        $post->excerpt = $excerpt;
+        $post->content = $content;
+        $post->category_id = $categoryId;
+        $post->author_id = User::where('role', 'author')->first()->id;
+
+        $post->save();
+
+        return redirect()->route('posts.index');
     }
 }
