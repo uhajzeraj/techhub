f31eb3a47efad1fd2329ec77dd1a9158b3e95669
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-30 11:14:46 +0100

    use Rule object

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -9,6 +9,7 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 final class PostsController
 {
@@ -48,7 +49,7 @@ final class PostsController
             'title' => ['required', 'string', 'min:5', 'max:100'],
             'excerpt' => ['required', 'string', 'min:5'],
             'content' => ['required', 'string', 'min:20'],
-            'category_id' => ['required', 'exists:categories,id'],
+            'category_id' => ['required', Rule::exists('categories', 'id')],
         ]);
 
         if ($validator->fails()) {
