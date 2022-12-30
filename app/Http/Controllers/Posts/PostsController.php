1773644aa1f4259599b7b89003b0b31a3580c716
uhajzeraj <uhajzeraj@hotmail.com>
2022-12-30 11:43:25 +0100

    use $request->validate directly

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -8,7 +8,6 @@ use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
 final class PostsController
@@ -45,20 +44,18 @@ final class PostsController
 
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $data = $request->validate([
             'title' => ['required', 'string', 'min:5', 'max:100'],
             'excerpt' => ['required', 'string', 'min:5'],
             'content' => ['required', 'string', 'min:20'],
             'category_id' => ['required', Rule::exists('categories', 'id')],
         ]);
 
-        $validator->validate();
-
-        $data = array_merge($validator->validated(), [
+        $data = array_merge($data, [
             'slug' => fake()->slug(),
             'author_id' => User::where('role', 'author')->first()->id,
         ]);
-        
+
         Post::create($data);
 
         return redirect()->route('posts.index');
