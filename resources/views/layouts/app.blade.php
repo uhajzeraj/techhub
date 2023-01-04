05bfb949026e13bd507d4117d01cae30f1ce586b
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-04 10:58:56 +0100

    create layout/app template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('title', config('app.name'))</title>
+    @yield('meta-tags')
+</head>
+
+<body>
+    <ul>
+        <li><a href="/">Homepage</a></li>
+        <li><a href="{{ route('posts.index') }}">Posts</a></li>
+    </ul>
+
+    @yield('content')
+
+    @if (session('success'))
+        <script>
+            alert('{{ session('success') }}');
+        </script>
+    @endif
+</body>
+
+</html>
