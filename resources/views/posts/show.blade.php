b525115370dce22caf5fcf34539ae815919fd55f
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-04 10:38:44 +0100

    make author name a link

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,7 +16,11 @@
 
     <h1>{{ $post->title }}</h1>
 
-    <h3>Written by: {{ $post->author->name }}</h3>
+    <h3>Written by:
+        <a href="{{ route('authors.show', $post->author->username) }}">
+            {{ $post->author->name }}
+        </a>
+    </h3>
 
     <div style="margin-bottom: 2em;">
         {{ $post->excerpt }}
