95faf6ede1e1414d8e5043579e87c3685e1d217f
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-06 11:17:25 +0100

    update request()->is() constraint

--- resources/views/components/layouts/navigation-link.blade.php
+++ resources/views/components/layouts/navigation-link.blade.php
@@ -1,6 +1,6 @@
 @props(['href'])
 
 <a href="{{ $href }}"
-    class="inline-flex items-center border-b-2 {{ request()->is(ltrim($href, '/')) ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500' }} px-1 pt-1 text-sm font-medium hover:border-gray-300 hover:text-gray-700">
+    class="inline-flex items-center border-b-2 {{ request()->is(ltrim($href, '/') . '*') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500' }} px-1 pt-1 text-sm font-medium hover:border-gray-300 hover:text-gray-700">
     {{ $slot }}
 </a>
