fae0afb500874b76d6ecbc652e45b9b8ce2ab4ab
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-06 11:59:56 +0100

    use random avatars for author

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -36,7 +36,7 @@
                                     <a href="#">
                                         <span class="sr-only">{{ $post->author->name }}</span>
                                         <img class="h-10 w-10 rounded-full"
-                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                            src="https://www.gravatar.com/avatar/{{ $post->author->id }}?s=32&d=identicon&r=PG"
                                             alt="">
                                     </a>
                                 </div>
