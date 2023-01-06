81f35ac2912cda0a7f2bae45d25299b19a6d49b2
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-06 11:19:49 +0100

    refactor authors/show.blade.php

--- resources/views/authors/show.blade.php
+++ resources/views/authors/show.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{ $author->name . ' - ' . config('app.name') }}</title>
-</head>
+@section('title', $author->name . ' - ' . config('app.name'))
 
-<body>
-    <ul>
-        <li><a href="/">Homepage</a></li>
-        <li><a href="{{ route('posts.index') }}">Posts</a></li>
-    </ul>
-
+@section('content')
     <h1>{{ $author->name }}</h1>
 
     <h2>Posts:</h2>
@@ -23,6 +12,4 @@
         </a>
         <article>{{ $post->excerpt }}</article>
     @endforeach
-</body>
-
-</html>
+@endsection
