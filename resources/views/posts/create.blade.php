f0efd4366cd51742659957ef0a49ce0b392b7aca
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-10 11:13:10 +0100

    use the newly-created for component

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,48 +1,4 @@
 <x-layouts.app>
     <x-slot:title>{{ 'Add a new post - ' . config('app.name') }}</x-slot:title>
-    <h3>Add a new post</h3>
-    <form method="post" action="{{ route('posts.store') }}">
-        @csrf
-        <label for="category">Category: </label>
-        <select name="category_id" id="category">
-            @foreach ($categories as $category)
-                <option value="{{ $category->id }}"
-                    {{ old('category_id') === (string) $category->id ? 'selected' : '' }}>
-                    {{ $category->name }}
-                </option>
-            @endforeach
-        </select>
-        </br>
-        @error('category_id')
-            <div style="color: red">
-                {{ $message }}
-            </div>
-        @enderror
-
-        <label for="title">Title: </label>
-        <input type="text" name="title" id="title" value="{{ old('title') }}" /><br />
-        @error('title')
-            <div style="color: red">
-                {{ $message }}
-            </div>
-        @enderror
-
-        <label for="excerpt">Excerpt: </label>
-        <textarea name="excerpt" id="excerpt">{{ old('excerpt') }}</textarea><br />
-        @error('excerpt')
-            <div style="color: red">
-                {{ $message }}
-            </div>
-        @enderror
-
-        <label for="content">Content: </label>
-        <textarea name="content" id="content">{{ old('content') }}</textarea><br />
-        @error('content')
-            <div style="color: red">
-                {{ $message }}
-            </div>
-        @enderror
-
-        <button type="submit">Save post</button>
-    </form>
+    <x-create-post-form :categories="$categories"></x-create-post-form>
 </x-layouts.app>
