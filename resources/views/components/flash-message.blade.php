fceb0e3293c5e8854a7392afac9df48b3a284c20
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-10 11:57:36 +0100

    build an actual flash-message component

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,8 @@
 @if (session('success'))
-    <script>
-        alert('{{ session('success') }}');
-    </script>
+    <div x-data="{ show: false }" x-init="show = true;
+    setTimeout(() => show = false, 2500)" x-show="show" x-cloak x-transition
+        class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded fixed bottom-5 right-8"
+        role="alert">
+        <span class="block sm:inline">{{ session('success') }}</span>
+    </div>
 @endif
