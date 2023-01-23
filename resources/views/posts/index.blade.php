b421e299a88d41fdc96c56bb3819a1eda9696ab6
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-23 12:08:12 +0100

    fill search input with query parameter if set

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
                 <label for="search" class="block text-sm font-medium text-gray-700">Search posts</label>
                 <form method="GET" action="{{ route('posts.index') }}">
                     <div class="relative mt-1 flex items-center">
-                        <input type="text" name="search" id="search" class="block w-full rounded-md border-gray-300 pr-12 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                        <input type="text" value="{{ request()->get('search', '') }}" name="search" id="search" class="block w-full rounded-md border-gray-300 pr-12 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                         <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
                         <kbd class="inline-flex items-center rounded border border-gray-200 px-2 font-sans text-sm font-medium text-gray-400">⌘K</kbd>
                         </div>
