a9afec1e3c94899a8af31118ee575a24cc206fd7
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-24 11:06:54 +0100

    add query scope

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -33,4 +33,21 @@ final class Post extends Model
     {
         $query->whereNotNull('published_at');
     }
+
+    public function scopeFilterBySearchTerm($query)
+    {
+        $request = request();
+
+        if ($request->has('search')) {
+            $query->where(function ($query) use ($request) {
+                $query->where('title', 'LIKE', "%{$request->get('search')}%")
+                    ->orWhere('content', 'LIKE', "%{$request->get('search')}%")
+                    ->orWhereIn('author_id', function ($query) use ($request) {
+                        $query->select('id')
+                            ->from('users')
+                            ->where('name', 'LIKE', "%{$request->get('search')}%");
+                    });
+            });
+        }
+    }
 }
