f3fff31c67d88d5a49708865f3611613d99cdeb4
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-25 11:45:51 +0100

    move route under the auth middleware

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,6 @@ Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.cre
 Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
 Route::get('/posts/{post:slug}', [PostsController::class, 'show'])->name('posts.show');
 
-Route::post('/posts/{post}/comments', [PostCommentsController::class, 'store'])->name('post-comments.store')
-    ->middleware('auth');
-
 Route::get('authors', GetAuthorsController::class)->name('authors.index');
 Route::get('authors/{author:username}', GetSingleAuthorController::class)->name('authors.show');
 
@@ -39,6 +36,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/posts/{post}/comments', [PostCommentsController::class, 'store'])->name('post-comments.store');
 });
 
 require __DIR__ . '/auth.php';
