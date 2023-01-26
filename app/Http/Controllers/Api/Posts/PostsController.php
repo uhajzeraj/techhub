5650b335a6c8a4bd9c82d7ece8ae5dfe5fcb33aa
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-26 11:39:23 +0100

    map manually the posts to a response

--- app/Http/Controllers/Api/Posts/PostsController.php
+++ app/Http/Controllers/Api/Posts/PostsController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers\Api\Posts;
 
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Collection;
 
 final class PostsController
 {
     public function index()
     {
-        return response()->json(
-            Post::query()
-                ->wherePublished()
-                ->get()
+        $posts = Post::query()
+            ->wherePublished()
+            ->get();
+
+        $postResponse = $posts->map(
+            fn (Post $post) =>
+            [
+                'id' => $post->id,
+                'author_id' => $post->author_id,
+                'title' => $post->title,
+                'content' => $post->content,
+                'created_at' => $post->created_at,
+            ]
         );
+        return response()->json($postResponse);
     }
 }
