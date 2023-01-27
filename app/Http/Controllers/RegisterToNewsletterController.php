edce9c6b6162e8520a6d26e362b68e45824de9f2
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-27 12:04:32 +0100

    Create RegisterToNewsletterController.php

---
+++ app/Http/Controllers/RegisterToNewsletterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Newsletters\NewsletterService;
+use Illuminate\Http\Request;
+
+final class RegisterToNewsletterController
+{
+    public function __construct(
+        private readonly NewsletterService $newsletterService,
+    ) {
+    }
+
+    public function __invoke(Request $request)
+    {
+        $data = [
+            'email' => 'uran@duck.com',
+        ];
+
+        $this->newsletterService->register($data['email']);
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success', 'Added to newsletter');
+    }
+}
