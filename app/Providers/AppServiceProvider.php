1e5b7338065096c118ee66f51912979e05247120
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-27 12:08:13 +0100

    add the necessary bindings to the container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,9 +8,12 @@ use App\Services\BatInterface;
 use App\Services\BatOne;
 use App\Services\BatTwo;
 use App\Services\Baz;
+use App\Services\Newsletters\MailchimpNewsletterService;
+use App\Services\Newsletters\NewsletterService;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +29,27 @@ class AppServiceProvider extends ServiceProvider
         // BatOne can be replaced with BatTwo, or any other class
         // that implementes the BatInterface interface
         $this->app->singleton(BatInterface::class, fn () => new BatOne());
+
+        $this->app->singleton(
+            NewsletterService::class,
+            fn () => $this->app->get(MailchimpNewsletterService::class),
+        );
+
+        $this->app->singleton(
+            ApiClient::class,
+            function () {
+                $apiClient = new ApiClient();
+
+                // Don't do this, this is a bad practice
+                // API keys, tokens, passwords should be stored in .env and never commited
+                $apiClient->setConfig([
+                    'apiKey' => '3ceec5dbbe7324f7624d3375d48afc87-us21',
+                    'server' => 'us21'
+                ]);
+
+                return $apiClient;
+            },
+        );
     }
 
     /**
