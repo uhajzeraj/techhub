256ee6e57d1cafd0a08ddb860d058de35f033a22
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-31 11:48:23 +0100

    delete old image

--- app/Http/Controllers/UpdateAvatarController.php
+++ app/Http/Controllers/UpdateAvatarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 final class UpdateAvatarController
 {
@@ -12,11 +13,15 @@ final class UpdateAvatarController
             'avatar' => ['required', 'image', 'max:1024'],
         ]);
 
+        $oldFilename = $request->user()->avatar;
+
         $filename = $request->file('avatar')->store('avatars', 'public');
 
         $request->user()->update([
             'avatar' => $filename,
         ]);
+
+        Storage::disk('public')->delete($oldFilename);
 
         return redirect()
             ->back()
