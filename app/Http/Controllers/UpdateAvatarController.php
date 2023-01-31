cc2fc068401676c9769aa204770c94ca91e9103b
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-31 11:20:31 +0100

    upload image instructions

--- app/Http/Controllers/UpdateAvatarController.php
+++ app/Http/Controllers/UpdateAvatarController.php
@@ -8,5 +8,8 @@ final class UpdateAvatarController
 {
     public function __invoke(Request $request)
     {
+        // Validate the request
+        // Store the image on a permanent location and give it a unique name
+        // In database, store the name of the file
     }
 }
