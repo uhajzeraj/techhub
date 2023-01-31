c37a46e8d44bebcc9b9a31e0674de5ab1cc1e876
uhajzeraj <uhajzeraj@hotmail.com>
2023-01-31 11:40:29 +0100

    Update upload-user-avatar.blade.php

--- resources/views/profile/partials/upload-user-avatar.blade.php
+++ resources/views/profile/partials/upload-user-avatar.blade.php
@@ -40,6 +40,13 @@
             </div>
         </div>
 
-        <x-primary-button>{{ __('Save') }}</x-primary-button>
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+            @if (session('status') === 'avatar-updated')
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600">{{ __('Saved.') }}</p>
+            @endif
+        </div>
     </form>
 </section>
