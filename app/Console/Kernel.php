bc7daa77dc2b176001a25bb0b75ebadf7ff9db7e
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-01 11:50:15 +0100

    schedule command to run daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteInactiveTokens;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command(DeleteInactiveTokens::class)->dailyAt('02:00');
         // $schedule->command('inspire')->hourly();
     }
 
