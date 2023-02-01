c6dc3a84debeabecb7d22b4c0aaf0d1ca64c63ea
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-01 11:02:07 +0100

    add query to delete inactive tokens

--- app/Console/Commands/DeleteInactiveTokens.php
+++ app/Console/Commands/DeleteInactiveTokens.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Laravel\Sanctum\PersonalAccessToken;
 
 final class DeleteInactiveTokens extends Command
 {
@@ -12,6 +13,17 @@ final class DeleteInactiveTokens extends Command
 
     public function handle()
     {
+        $numberOfTokensDeleted = PersonalAccessToken::query()
+            ->where(function ($query) {
+                $query->whereNull('last_used_at')
+                    ->where('created_at', '<', now()->subMonth());
+            })
+            ->orWhere(function ($query) {
+                $query->whereNotNull('last_used_at')
+                    ->where('last_used_at', '<', now()->subMonth());
+            })
+            ->delete();
+
         return Command::SUCCESS;
     }
 }
