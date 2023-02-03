25c1e76317cf71698f637a9e39c74f5d32c88b31
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-03 11:09:48 +0100

    assertOk -> assertRedirect

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
@@ -16,6 +17,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect('/posts');
     }
 }
