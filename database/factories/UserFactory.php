943c16aa89157bdc754135190a08926ea6f2401f
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-03 11:32:49 +0100

    use bcrypt with 4 rounds in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'username' => Str::slug($name),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$04$QLAwhhoBVKHXX4RAB6ukSehYOs4VG2DpRQsj.YoMtxVg42S32u7cm', // password
             'remember_token' => Str::random(10),
         ];
     }
