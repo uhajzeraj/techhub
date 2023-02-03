05706a66cc0549b71d2c40de68be7057c2f6fc2d
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-03 10:55:56 +0100

    use @test annotation

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,11 +7,9 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_that_true_is_true()
+    public function true_is_true(): void
     {
         $this->assertTrue(true);
     }
