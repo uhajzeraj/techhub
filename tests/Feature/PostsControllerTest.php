e061af747e6109eb0859f368ff374f337ad9591d
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-06 11:45:18 +0100

    add test for deleting post

--- tests/Feature/PostsControllerTest.php
+++ tests/Feature/PostsControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -38,6 +39,28 @@ final class PostsControllerTest extends TestCase
                 'title' => 'My post title',
                 'excerpt' => 'My post excerpt',
                 'content' => 'This is my content and it is longer than 20 characters',
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanDeleteAnExistingPost(): void
+    {
+        // Arrange
+        $this->withoutExceptionHandling();
+
+        $post = Post::factory()->create();
+
+        // Act
+        $response = $this->delete("/posts/{$post->id}");
+
+        // Assert
+        $response->assertRedirect('/posts');
+
+        $this->assertDatabaseCount('posts', 0)
+            ->assertDatabaseMissing('posts', [
+                'id' => $post->id,
             ]);
     }
 }
