7997c136b3bcc8882d33fb847f95382ac9784cc2
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-06 11:35:41 +0100

    add test implementation

--- tests/Feature/PostsControllerTest.php
+++ tests/Feature/PostsControllerTest.php
@@ -17,5 +17,19 @@ final class PostsControllerTest extends TestCase
     {
         // Arrange
         $this->withoutExceptionHandling();
+
+        $category = Category::factory()->create();
+        $author = User::factory()->author()->create();
+
+        // Act
+        $response = $this->post('/posts', [
+            'category_id' => $category->id,
+            'title' => 'My post title',
+            'excerpt' => 'My post excerpt',
+            'content' => 'This is my content and it is longer than 20 characters',
+        ]);
+
+        // Assert
+        $response->assertRedirect('/posts');
     }
 }
