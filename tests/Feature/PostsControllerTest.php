d0ea245eacdfcbb85b82edacde3e1234da8107f9
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-06 11:32:30 +0100

    add test case

--- tests/Feature/PostsControllerTest.php
+++ tests/Feature/PostsControllerTest.php
@@ -2,10 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 final class PostsControllerTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function itCanCreateANewPost(): void
+    {
+    }
 }
