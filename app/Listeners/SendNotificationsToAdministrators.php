5252c753a846f6bd14d0e51fde4b06a4bc2ef9a9
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-06 11:09:50 +0100

    actually send the email

--- app/Listeners/SendNotificationsToAdministrators.php
+++ app/Listeners/SendNotificationsToAdministrators.php
@@ -3,12 +3,17 @@
 namespace App\Listeners;
 
 use App\Events\PostWasCreatedEvent;
+use App\Mail\PostWasCreated;
 use App\Models\Post;
+use Illuminate\Support\Facades\Mail;
 
 final class SendNotificationsToAdministrators
 {
     public function handle(PostWasCreatedEvent $event): void
     {
         $post = Post::query()->find($event->postId);
+
+        Mail::to($post->author->email)
+            ->send(new PostWasCreated($post));
     }
 }
