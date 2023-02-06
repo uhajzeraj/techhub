072146fa8030efb794611000c7c064de8dbef130
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-06 11:13:12 +0100

    inject dependency instead of using facade

--- app/Listeners/SendNotificationsToAdministrators.php
+++ app/Listeners/SendNotificationsToAdministrators.php
@@ -5,15 +5,20 @@ namespace App\Listeners;
 use App\Events\PostWasCreatedEvent;
 use App\Mail\PostWasCreated;
 use App\Models\Post;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Contracts\Mail\Mailer;
 
 final class SendNotificationsToAdministrators
 {
+    public function __construct(
+        private readonly Mailer $mailer,
+    ) {
+    }
+
     public function handle(PostWasCreatedEvent $event): void
     {
         $post = Post::query()->find($event->postId);
 
-        Mail::to($post->author->email)
+        $this->mailer->to($post->author->email)
             ->send(new PostWasCreated($post));
     }
 }
