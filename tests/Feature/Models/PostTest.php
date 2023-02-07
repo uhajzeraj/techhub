e54c30e1c8c366a6147171a5a6e12fbe27cf7580
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-07 11:14:55 +0100

    test it belongs to a category

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Tests\TestCase;
@@ -17,5 +18,16 @@ final class PostTest extends TestCase
         $post = Post::factory()->create(['author_id' => $author->id]);
 
         $this->assertTrue($post->author->is($author));
+    }
+
+    /**
+     * @test
+     */
+    public function itBelongsToACategory(): void
+    {
+        $category = Category::factory()->create();
+        $post = Post::factory()->create(['category_id' => $category->id]);
+
+        $this->assertTrue($post->category->is($category));
     }
 }
