f4e427579a63223628ad8e9614340a6e2712a8ac
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-07 11:21:41 +0100

    test it has published posts

--- tests/Feature/Models/UserTest.php
+++ tests/Feature/Models/UserTest.php
@@ -29,4 +29,28 @@ final class UserTest extends TestCase
         $this->assertTrue($user->allPosts->contains($post2));
         $this->assertFalse($user->allPosts->contains($post3));
     }
+
+    /**
+     * @test
+     */
+    public function hasManyPublishedPosts(): void
+    {
+        $user = User::factory()->author()->create();
+        $otherUser = User::factory()->author()->create();
+
+        [$post1, $post2, $post3, $post4] = Post::factory(4)
+            ->sequence(
+                ['author_id' => $user->id, 'published_at' => now()],
+                ['author_id' => $user->id, 'published_at' => now()],
+                ['author_id' => $otherUser->id, 'published_at' => now()],
+                ['author_id' => $user->id],
+            )
+            ->create();
+
+        $this->assertCount(2, $user->posts);
+        $this->assertTrue($user->posts->contains($post1));
+        $this->assertTrue($user->posts->contains($post2));
+        $this->assertFalse($user->posts->contains($post3));
+        $this->assertFalse($user->posts->contains($post4));
+    }
 }
