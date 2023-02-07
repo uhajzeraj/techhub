696ac0316177510a17a83be15a0fa1c91d79e5cd
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-07 11:13:41 +0100

    test post belongs to an author

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -2,8 +2,20 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Post;
+use App\Models\User;
 use Tests\TestCase;
 
 final class PostTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function itBelongsToAnAuthor(): void
+    {
+        $author = User::factory()->author()->create();
+        $post = Post::factory()->create(['author_id' => $author->id]);
+
+        $this->assertTrue($post->author->is($author));
+    }
 }
