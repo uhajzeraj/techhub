4852b536cf1455307744c852d38380210c15ded9
uhajzeraj <uhajzeraj@hotmail.com>
2023-02-07 11:38:05 +0100

    test post has many tags

--- tests/Feature/Models/PostTest.php
+++ tests/Feature/Models/PostTest.php
@@ -4,7 +4,9 @@ namespace Tests\Feature\Models;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 final class PostTest extends TestCase
@@ -29,5 +31,28 @@ final class PostTest extends TestCase
         $post = Post::factory()->create(['category_id' => $category->id]);
 
         $this->assertTrue($post->category->is($category));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanHaveManyTags(): void
+    {
+        $post = Post::factory()->create();
+        $otherPost = Post::factory()->create();
+
+        [$tag1, $tag2, $tag3] = Tag::factory(3)->create();
+
+        DB::table('post_tags')->insert([
+            ['post_id' => $post->id, 'tag_id' => $tag1->id],
+            ['post_id' => $post->id, 'tag_id' => $tag2->id],
+            ['post_id' => $otherPost->id, 'tag_id' => $tag2->id],
+            ['post_id' => $otherPost->id, 'tag_id' => $tag3->id],
+        ]);
+
+        $this->assertCount(2, $post->tags);
+        $this->assertTrue($post->tags->contains($tag1));
+        $this->assertTrue($post->tags->contains($tag2));
+        $this->assertFalse($post->tags->contains($tag3));
     }
 }
